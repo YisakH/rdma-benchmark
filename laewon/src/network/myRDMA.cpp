7ebfdf94eae896a964de7393c97c62f95dc0f996
YisakH <drm05003@naver.com>
2022-04-14 23:01:30 +0900

    부하 줄여보자 2 -> 메시지 1000개 보내기

--- laewon/src/network/myRDMA.cpp
+++ laewon/src/network/myRDMA.cpp
@@ -6,7 +6,7 @@
 
 #define MAX_SEND_BYTES static_cast<long>(10737418240) // 10GB
 #define MAX_MSG_SIZE 8388608
-#define MAX_ITERATION 100000
+#define MAX_ITERATION 1000
 
 uint64_t timeDiff(struct timeval stop, struct timeval start)
 {
