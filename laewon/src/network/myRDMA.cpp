bcee2a7f039c5b43dc4654c9c1cb7cfab0da32ad
YisakH <drm05003@naver.com>
2022-04-14 23:17:43 +0900

    메시지 1000개 -> 1000000개

--- laewon/src/network/myRDMA.cpp
+++ laewon/src/network/myRDMA.cpp
@@ -4,9 +4,9 @@
 #include <time.h>
 #include <sys/time.h>
 
-#define MAX_SEND_BYTES static_cast<long>(107374182400) // 10GB
+#define MAX_SEND_BYTES static_cast<long>(107374182400) // 100GB
 #define MAX_MSG_SIZE 8388608
-#define MAX_ITERATION 10000
+#define MAX_ITERATION 1000000
 
 uint64_t timeDiff(struct timeval stop, struct timeval start)
 {
